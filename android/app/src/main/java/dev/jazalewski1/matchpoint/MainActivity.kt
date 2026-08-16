675312f8e8291b44cd56585b7a54e55f35706711
Jakub Zalewski <52531410+jazalewski1@users.noreply.github.com>
2026-08-16 23:39:05 +0200

    feat: Add dummy home screen and launcher icon (#1)

---
+++ android/app/src/main/java/dev/jazalewski1/matchpoint/MainActivity.kt
@@ -0,0 +1,20 @@
+package dev.jazalewski1.matchpoint
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import dev.jazalewski1.matchpoint.core.ui.theme.AppTheme
+import dev.jazalewski1.matchpoint.feature.home.HomeScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AppTheme {
+                HomeScreen()
+            }
+        }
+    }
+}
---
+++ android/feature/home/src/main/java/dev/jazalewski1/matchpoint/feature/home/HomeScreen.kt
@@ -0,0 +1,56 @@
+package dev.jazalewski1.matchpoint.feature.home
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import dev.jazalewski1.matchpoint.core.ui.theme.AppTheme
+
+@Composable
+fun HomeScreen() {
+    Scaffold { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Column(
+                modifier = Modifier.weight(0.5f),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(
+                    text = "Matchpoint",
+                    style = MaterialTheme.typography.displayLarge,
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(0.6f)
+                    .weight(0.5f),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Button(
+                    onClick = {},
+                    modifier = Modifier.fillMaxWidth(),
+                ) {
+                    Text(
+                        text = "Start new match",
+                        style = MaterialTheme.typography.bodyLarge,
+                    )
+                }
+            }
+        }
+    }
+}
+
+@PreviewLightDark
+@Composable
+private fun HomeScreenPreview() {
+    AppTheme {
+        HomeScreen()
+    }
+}
