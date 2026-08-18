f1b4c54b292e0a5820816f35e67a99af3b9e4ff6
Jakub Zalewski <jazalewski1@gmail.com>
2026-08-18 22:26:16 +0200

    Add logo to home screen

--- android/feature/home/src/main/java/dev/jazalewski1/matchpoint/feature/home/HomeScreen.kt
+++ android/feature/home/src/main/java/dev/jazalewski1/matchpoint/feature/home/HomeScreen.kt
@@ -1,10 +1,12 @@
 package dev.jazalewski1.matchpoint.feature.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import dev.jazalewski1.matchpoint.core.ui.theme.AppTheme
 
@@ -12,7 +14,9 @@ import dev.jazalewski1.matchpoint.core.ui.theme.AppTheme
 fun HomeScreen() {
     Scaffold { innerPadding ->
         Column(
-            modifier = Modifier.padding(innerPadding).fillMaxSize(),
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Column(
@@ -20,10 +24,15 @@ fun HomeScreen() {
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally,
             ) {
-                Text(text = "Matchpoint", style = MaterialTheme.typography.displayLarge)
+                Image(
+                    painter = painterResource(R.drawable.logo_full),
+                    contentDescription = "Matchpoint Logo"
+                )
             }
             Column(
-                modifier = Modifier.fillMaxWidth(0.6f).weight(0.5f),
+                modifier = Modifier
+                    .fillMaxWidth(0.6f)
+                    .weight(0.5f),
                 horizontalAlignment = Alignment.CenterHorizontally,
             ) {
                 Button(onClick = {}, modifier = Modifier.fillMaxWidth()) {
