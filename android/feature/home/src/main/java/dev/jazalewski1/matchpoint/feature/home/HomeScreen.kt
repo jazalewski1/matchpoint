c86f586c1fae575de0b8195118b98ec28c8dd350
Jakub Zalewski <jazalewski1@gmail.com>
2026-08-18 23:08:44 +0200

    Update theme and colors

--- android/feature/home/src/main/java/dev/jazalewski1/matchpoint/feature/home/HomeScreen.kt
+++ android/feature/home/src/main/java/dev/jazalewski1/matchpoint/feature/home/HomeScreen.kt
@@ -6,8 +6,12 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Devices.DEFAULT
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
 import dev.jazalewski1.matchpoint.core.ui.theme.AppTheme
 
 @Composable
@@ -47,4 +51,40 @@ fun HomeScreen() {
 @Composable
 private fun HomeScreenPreview() {
     AppTheme { HomeScreen() }
+}
+
+@Composable
+private fun PreviewSurface(
+    text: String,
+    color: Color,
+) {
+    Surface(
+        modifier = Modifier.padding(4.dp).fillMaxWidth(),
+        color = color,
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier.padding(8.dp),
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true, device = DEFAULT)
+@Composable
+private fun JustPreview() {
+    AppTheme {
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            PreviewSurface("primary", MaterialTheme.colorScheme.primary)
+            PreviewSurface("primaryContainer", MaterialTheme.colorScheme.primaryContainer)
+            PreviewSurface("secondary", MaterialTheme.colorScheme.secondary)
+            PreviewSurface("secondaryContainer", MaterialTheme.colorScheme.secondaryContainer)
+            PreviewSurface("tertiary", MaterialTheme.colorScheme.tertiary)
+            PreviewSurface("tertiaryContainer", MaterialTheme.colorScheme.tertiaryContainer)
+            PreviewSurface("error", MaterialTheme.colorScheme.error)
+            PreviewSurface("errorContainer", MaterialTheme.colorScheme.errorContainer)
+        }
+    }
 }
