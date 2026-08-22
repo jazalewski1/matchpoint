05350b3291cb9f7531505acfa050b225a9ccded3
Jakub Zalewski <jazalewski1@gmail.com>
2026-08-22 22:49:17 +0200

    Format code

--- android/feature/match/src/main/java/dev/jazalewski1/matchpoint/feature/match/MatchScreen.kt
+++ android/feature/match/src/main/java/dev/jazalewski1/matchpoint/feature/match/MatchScreen.kt
@@ -5,7 +5,6 @@ import android.content.pm.ActivityInfo
 import androidx.compose.animation.Animatable
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.FastOutSlowInEasing
-import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
