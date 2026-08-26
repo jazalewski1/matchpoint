e62d1a6a5167405553b824470a5b4a215168cce9
Jakub Zalewski <jazalewski1@gmail.com>
2026-08-26 16:39:45 +0200

    Evaluate game with Game class

--- android/domain/tennis/src/main/java/dev/jazalewski1/matchpoint/domain/tennis/MatchControllerImpl.kt
+++ android/domain/tennis/src/main/java/dev/jazalewski1/matchpoint/domain/tennis/MatchControllerImpl.kt
@@ -1,69 +1,111 @@
 package dev.jazalewski1.matchpoint.domain.tennis
 
 class MatchControllerImpl : MatchController {
-    private var gameState: GameState = GameState.default()
+    private val game = Game()
 
-    override fun getCurrentGame() = gameState
-
-    override fun addPointToLhs(): PointOutcome =
-        when (val current = gameState) {
-            is GameState.Regular -> {
-                val nextLhs = current.lhs.next()
-                if (nextLhs != null) {
-                    if (nextLhs == Points.FORTY && current.rhs == Points.FORTY) {
-                        gameState = GameState.Deuce
-                        return PointOutcome.PointScored
-                    } else {
-                        gameState = current.copy(lhs = nextLhs)
-                        return PointOutcome.PointScored
-                    }
-                } else {
-                    gameState = GameState.default()
-                    return PointOutcome.GameWon
+    override fun getCurrentGame() =
+        when (val gameState = game.state) {
+            is Game.State.Regular ->
+                GameState.Regular(
+                    lhs = gameState.player1,
+                    rhs = gameState.player2,
+                )
+            is Game.State.Deuce -> GameState.Deuce
+            is Game.State.Advantage ->
+                when (gameState.player) {
+                    Player.ONE -> GameState.Advantage.Lhs
+                    Player.TWO -> GameState.Advantage.Rhs
                 }
-            }
-            is GameState.Deuce -> {
-                gameState = GameState.Advantage.Lhs
-                return PointOutcome.PointScored
-            }
-            is GameState.Advantage.Lhs -> {
-                gameState = GameState.default()
-                return PointOutcome.GameWon
-            }
-            is GameState.Advantage.Rhs -> {
-                gameState = GameState.Deuce
-                return PointOutcome.PointScored
-            }
         }
 
-    override fun addPointToRhs(): PointOutcome =
-        when (val current = gameState) {
-            is GameState.Regular -> {
-                val nextRhs = current.rhs.next()
-                if (nextRhs != null) {
-                    if (nextRhs == Points.FORTY && current.lhs == Points.FORTY) {
-                        gameState = GameState.Deuce
-                        return PointOutcome.PointScored
-                    } else {
-                        gameState = current.copy(rhs = nextRhs)
-                        return PointOutcome.PointScored
-                    }
-                } else {
-                    gameState = GameState.default()
-                    return PointOutcome.GameWon
-                }
-            }
-            is GameState.Deuce -> {
-                gameState = GameState.Advantage.Rhs
-                return PointOutcome.PointScored
-            }
-            is GameState.Advantage.Lhs -> {
-                gameState = GameState.Deuce
-                return PointOutcome.PointScored
-            }
-            is GameState.Advantage.Rhs -> {
-                gameState = GameState.default()
-                return PointOutcome.GameWon
+    override fun addPointToLhs(): PointOutcome {
+        return when (game.addPointToPlayer1()) {
+            is GameOutcome.PointScored -> PointOutcome.PointScored
+            is GameOutcome.Finished -> {
+                game.reset()
+                PointOutcome.GameWon
             }
         }
+    }
+
+    override fun addPointToRhs(): PointOutcome {
+        return when (game.addPointToPlayer2()) {
+            is GameOutcome.PointScored -> PointOutcome.PointScored
+            is GameOutcome.Finished -> {
+                game.reset()
+                PointOutcome.GameWon
+            }
+        }
+    }
+}
+
+private enum class Player {
+    ONE,
+    TWO,
+}
+
+private sealed interface GameOutcome {
+    data class PointScored(val winner: Player) : GameOutcome
+
+    data class Finished(val winner: Player) : GameOutcome
+}
+
+private class Game {
+    sealed interface State {
+        fun next(pointWinner: Player): State?
+
+        data class Regular(val player1: Points, val player2: Points) : State {
+            override fun next(pointWinner: Player): State? =
+                when (pointWinner) {
+                    Player.ONE -> {
+                        player1.next()?.let { nextPlayer1 ->
+                            if (nextPlayer1 == Points.FORTY && player2 == Points.FORTY) {
+                                return Deuce
+                            }
+                            this.copy(player1 = nextPlayer1)
+                        }
+                    }
+                    Player.TWO -> {
+                        player2.next()?.let { nextPlayer2 ->
+                            if (nextPlayer2 == Points.FORTY && player1 == Points.FORTY) {
+                                return Deuce
+                            }
+                            this.copy(player2 = nextPlayer2)
+                        }
+                    }
+                }
+        }
+
+        data object Deuce : State {
+            override fun next(pointWinner: Player) = Advantage(player = pointWinner)
+        }
+
+        data class Advantage(val player: Player) : State {
+            override fun next(pointWinner: Player): State? {
+                if (pointWinner == player) {
+                    return null
+                }
+                return Deuce
+            }
+        }
+    }
+
+    var state: State = State.Regular(Points.LOVE, Points.LOVE)
+        private set
+
+    fun addPointToPlayer1(): GameOutcome = addPoint(Player.ONE)
+
+    fun addPointToPlayer2(): GameOutcome = addPoint(Player.TWO)
+
+    fun reset() {
+        state = State.Regular(Points.LOVE, Points.LOVE)
+    }
+
+    private fun addPoint(player: Player): GameOutcome {
+        state.next(pointWinner = player)?.let { nextState ->
+            state = nextState
+            return GameOutcome.PointScored(winner = player)
+        }
+        return GameOutcome.Finished(winner = player)
+    }
 }
